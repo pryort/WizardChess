beb8762346185f3fcef2a28ad073be7181ae2fd8
pryort <pryort@purdue.edu>
2025-11-06 10:39:07 -0500

    i2c

--- main/main.c
+++ main/main.c
@@ -20,10 +20,11 @@ void app_main()
     uint8_t tx_data[] = {0xA1, 0xB2, 0xC3};
     i2c_comm_write(0x67, tx_data, sizeof(tx_data));
 
-    vTaskDelay(pdMS_TO_TICKS(500));
+    vTaskDelay(pdMS_TO_TICKS(10));
 
     uint8_t rx_data[3] = {0};
     i2c_comm_read(0x67, rx_data, sizeof(rx_data));
+    printf("%X %X %X\n", rx_data[0], rx_data[1], rx_data[2]);
 
     ESP_LOGI(TAG, "I2C read complete: %02X %02X %02X", rx_data[0], rx_data[1], rx_data[2]);
     // Lock the mutex
